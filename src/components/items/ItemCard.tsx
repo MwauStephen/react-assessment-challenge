5bb03330628b041e88e42d6c6e54a44c02268334
krisine <kristine.nyaga@student.moringaschool.com>
2025-08-11 19:20:40 +0300

    add  ItemCard component for displaying a Pokemon

--- src/components/items/ItemCard.tsx
+++ src/components/items/ItemCard.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-const ItemCard = () => {
-  return (
-    <div>ItemCard</div>
-  )
+interface ItemCardProps {
+  name: string;
+  id: string | number;
 }
 
-export default ItemCard
+const ItemCard: React.FC<ItemCardProps> = ({ name, id }) => {
+  const imageUrl =`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`
+  return (
+    <div
+      className="bg-white p-4 rounded-lg shadow hover:shadow-lg hover:scale-105"
+    >
+      <div className="img-wrap flex justify-center items-center h-28">
+        <Image
+          width={500}
+          height={500}
+          src={imageUrl}
+          alt={name}
+          className="max-h-28 object-contain"
+        />
+      </div>
+      <h2 className="text-center text-lg font-medium capitalize mt-5">{name}</h2>
+    </div>
+  );
+};
+
+export default ItemCard;
