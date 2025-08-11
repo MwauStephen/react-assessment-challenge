59f401f6dbe1349a84b6252facecb75db2923388
krisine <kristine.nyaga@student.moringaschool.com>
2025-08-11 22:58:21 +0300

    add link to item details page

--- src/components/items/ItemCard.tsx
+++ src/components/items/ItemCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 interface ItemCardProps {
@@ -9,20 +10,22 @@ interface ItemCardProps {
 const ItemCard: React.FC<ItemCardProps> = ({ name, id }) => {
   const imageUrl =`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`
   return (
-    <div
-      className="bg-white p-4 rounded-lg shadow hover:shadow-lg hover:scale-105"
-    >
-      <div className="img-wrap flex justify-center items-center h-28">
-        <Image
-          width={500}
-          height={500}
-          src={imageUrl}
-          alt={name}
-          className="max-h-28 object-contain"
-        />
+    <Link href={`/items/${id}`}>
+      <div className="bg-white p-4 cursor-pointer rounded-lg shadow hover:shadow-lg hover:scale-105 transition-transform duration-200">
+        <div className="img-wrap flex justify-center items-center h-28">
+          <Image
+            width={500}
+            height={500}
+            src={imageUrl}
+            alt={name}
+            className="max-h-28 object-contain"
+          />
+        </div>
+        <h2 className="text-center text-lg font-medium capitalize mt-5">
+          {name}
+        </h2>
       </div>
-      <h2 className="text-center text-lg font-medium capitalize mt-5">{name}</h2>
-    </div>
+    </Link>
   );
 };
 
