bf1abfa5457cd73ce9a483d6dfc48b46cd4b7835
krisine <kristine.nyaga@student.moringaschool.com>
2025-08-11 21:12:56 +0300

    add function to fetch one pokemon

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,3 @@
-
 export interface PokemonListResult {
   name: string;
   url: string;
@@ -11,14 +10,44 @@ export interface PokemonListResponse {
   results: PokemonListResult[];
 }
 
+export interface PokemonDetailResponse {
+  id: number;
+  name: string;
+  height: number;
+  weight: number;
+  sprites: {
+    other: {
+      dream_world: { front_default: string };
+      ["official-artwork"]: { front_default: string };
+    };
+  };
+  types: { slot: number; type: { name: string } }[];
+  abilities: { ability: { name: string } }[];
+  stats: { base_stat: number; stat: { name: string } }[];
+  base_experience:number
+}
+
 const BASE_URL = "https://pokeapi.co/api/v2";
 
-export async function getPokemonList(limit = 20, offset = 0): Promise<PokemonListResponse> {
-  const res = await fetch(`${BASE_URL}/pokemon?limit=${limit}&offset=${offset}`);
 
+export async function getPokemonList(
+  limit = 20,
+  offset = 0
+): Promise<PokemonListResponse> {
+  const res = await fetch(`${BASE_URL}/pokemon?limit=${limit}&offset=${offset}`);
   if (!res.ok) {
     throw new Error(`Failed to fetch Pokémon list`);
   }
+  return res.json();
+}
 
+
+export async function getPokemonDetail(
+  id: string | number
+): Promise<PokemonDetailResponse> {
+  const res = await fetch(`${BASE_URL}/pokemon/${id}`);
+  if (!res.ok) {
+    throw new Error(`Failed to fetch Pokémon details`);
+  }
   return res.json();
 }
